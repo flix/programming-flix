704f64e9d767018e0605faa899050fc09939c371
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-03-17 20:52:33 +0100

    Expanded on concurrency.

--- src/content/Concurrency.js
+++ src/content/Concurrency.js
@@ -6,6 +6,8 @@ import Section from "../components/Section";
 import SubSection from "../components/SubSection";
 import Warning from "../components/Warning";
 import PlannedFeature from "../components/PlannedFeature";
+import SubSubSection from "../components/SubSubSection";
+import DesignNote from "../components/DesignNote";
 
 class Concurrency extends React.Component {
 
@@ -83,6 +85,110 @@ def main(): Int =
                     </PlannedFeature>
 
                 </SubSection>
+
+                <SubSection name="Selecting on Channels">
+
+                    <p>
+                        We can use the <Code>select</Code> expression to receive a message from a collection of
+                        channels. For example:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`def meow(c: Channel[Str]): Unit = c <- "Meow!"; ()
+def woof(c: Channel[Str]): Unit = c <- "Woof!"; ()
+def main(): Str =
+    let c1 = chan Str 1;
+    let c2 = chan Str 1;
+    spawn meow(c1);
+    spawn woof(c1);
+    select {
+        case m <- c1 => m
+        case m <- c2 => m
+    }`}
+                    </Editor>
+
+                    <p>
+                        Many important concurrency patterns such as producer-consumer and load balancers can be
+                        expressed with the <Code>select</Code> expression.
+                    </p>
+
+                    <Warning>
+                        The absence of locks in Flix does not prevent deadlocks! A process can easily deadlock, e.g. by
+                        reading from a channel that is never written to!
+                    </Warning>
+
+                </SubSection>
+
+                <SubSubSection name="Selecting with Default">
+
+                    <p>
+                        In some cases we do not want to block forever until a message arrives, but instead prefer to
+                        take some alternative action if no message is readily available. We can achieve this with a
+                        default case:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`def main(): Str =
+    let c1 = chan Str 1;
+    let c2 = chan Str 1;
+    select {
+        case m <- c1 => "one"
+        case m <- c1 => "two"
+        case _       => "default"
+    }`}
+                    </Editor>
+
+                    <p>
+                        Here a message is never sent to <Code>c1</Code> nor <Code>c2</Code>.
+                        The <Code>select</Code> expression tries all cases in order, and if no channel is ready, it
+                        immediately selects the default case. Hence using a default case prevents
+                        the <Code>select</Code> expression from (potentially) blocking.
+                    </p>
+
+                </SubSubSection>
+
+                <SubSubSection name="Selecting with Tickers and Timers">
+
+                    <p>
+                        As an alternative to a default case, we can use <i>tickers</i> and <i>timers</i> to wait for
+                        pre-defined periods of time inside a <Code>select</Code> expression.
+                    </p>
+
+                    <p>
+                        For example, here is a program that has a slow function that takes a minute to send a message on
+                        a channel, but the <Code>select</Code> expression relies on <Code>Timer.seconds</Code> to only
+                        wait <Code>5</Code> seconds before giving up:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`def slow(c: Channel[Str]): Unit =
+    sleep(Duration.oneMinute());
+    c <- "I am very slow";
+    ()
+
+def main(): Str =
+    let c = chan Str 1;
+    spawn slow(c);
+    select {
+        case m <- c                    => m
+        case t <- Timer.seconds(5i64)  => "timeout"
+    }
+`}
+                    </Editor>
+
+                    <DesignNote>
+                        The argument to <Code>Timer.seconds</Code> requires a 64-bit integer, so for now we have to
+                        specify it with the <Code>i64</Code> suffix, since otherwise <Code>5</Code> would be interpreted
+                        as a 32-bit number and we would get a type error. In the future, we plan to have some form of
+                        integer promotion.
+                    </DesignNote>
+
+                    <p>
+                        This program returns the string <Code>"timeout"</Code> after five seconds, but terminates after
+                        one minute when the last process terminates (!)
+                    </p>
+
+                </SubSubSection>
 
             </Section>
         )

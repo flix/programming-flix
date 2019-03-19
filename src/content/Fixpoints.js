58c25a7fd1db7249668b050186b42e2f697240eb
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2019-03-19 08:49:00 +0100

    Work on Fixpoint section.

--- src/content/Fixpoints.js
+++ src/content/Fixpoints.js
@@ -18,11 +18,10 @@ class Introduction extends React.Component {
                     relations</i> and <i>constraint on lattices</i>.
                 </p>
 
-                <SubSection name="Using Flix as a Datalog Solver">
+                <SubSection name="Using Flix to Solve Constraints on Relations">
 
                     <p>
-                        We can use Flix as an ordinary Datalog solver by writing relational constraints at the
-                        top-level:
+                        We can use Flix to solve fixpoints on relational constraints written at the top-level:
                     </p>
 
                     <Editor flix={this.props.flix}>
@@ -51,10 +50,39 @@ Connected(x, z) :- Connected(x, y), DirectedEdge(y, z).`}
                         from <Code>y</Code> to <Code>z</Code>.
                     </p>
 
+                    <p>
+                        Note: The expression "fixpoints on relational constraints" is equivalent to Datalog. That is,
+                        when used in this way Flix works as an ordinary Datalog solver.
+                    </p>
+
                     <DesignNote>
                         In Flix, all predicate symbols must be declared before they can be used. We are currently
                         debating whether or not this is a good thing.
-                        </DesignNote>
+                    </DesignNote>
+
+                </SubSection>
+
+                <SubSection name="Programming with First-class Constraints">
+
+                    <p>
+                        A unique feature of Flix is its support for <i>first-class constraints</i>.
+                    </p>
+
+
+
+
+                </SubSection>
+
+                <SubSection name="Using Flix to Solve Constraints on Lattices">
+
+                    <p>
+                        Flix generalizes Datalog to support <i>constraints on lattices</i>:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`....
+`}
+                    </Editor>
 
                 </SubSection>
 

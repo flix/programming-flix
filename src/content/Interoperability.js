33589a7ff8ab6a4a2a2c0250b86e8ad0444888e9
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2020-03-22 12:20:29 +0100

    Added subsection on Creating Objects.

--- src/content/Interoperability.js
+++ src/content/Interoperability.js
@@ -5,6 +5,7 @@ import Code from '../components/Code';
 import Editor from '../util/Editor';
 import Section from "../components/Section";
 import SubSection from "../components/SubSection";
+import DesignNote from "../components/DesignNote";
 
 class Interoperability extends React.Component {
 
@@ -18,17 +19,57 @@ class Interoperability extends React.Component {
             <Section name="Interoperability">
 
                 <p>
-                    Flix supports interoperability with Java through the <Code>import</Code> mechanism.
+                    Flix supports interoperability with Java through the <Code>import</Code> mechanism. The import
+                    mechanism allow us to locally refer to a Java constructor, method, or field.
                 </p>
 
                 <SubSection name="Creating Objects">
+
                     <p>
-                        TBD
+                        We can create a new Java object of a given class as follows:
                     </p>
 
                     <Editor flix={this.props.flix}>
-                        def main(): List[Int] = Nil
+                        {`def main(): ##java.io.File & Impure =
+    import new java.io.File(String) as newFile;
+    newFile("helloworld.txt")`}
                     </Editor>
+
+                    <p>
+                        Here we import the constructor of the <Code>java.io.File</Code> class and give it the local
+                        name <Code>newFile</Code>. <Code>newFile</Code> is an impure function that takes
+                        a <Code>String</Code> and returns a Java <Code>File</Code> object. The return type
+                        of <Code>main</Code> is such a file object which is indicated by the Java
+                        type <Code>java.io.File</Code>.
+                    </p>
+
+                    <DesignNote>
+                        At the moment all Java types must be fully-qualified. This may be simplified in the future.
+                    </DesignNote>
+
+                    <p>
+                        The <Code>java.io.File</Code> class has another construct that takes two arguments: one for
+                        parent pathname and one for the child pathname. We can use this constructor as follows:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`def main(): ##java.io.File & Impure =
+    import new java.io.File(String, String) as newFile;
+    newFile("text", "helloworld.txt")`}
+                    </Editor>
+
+                    <p>
+                        All interoperability constructs are impure, since Java is an impure language. If you call a
+                        function which you know to be pure, you can cast it from impure to pure, as the following
+                        example shows:
+                    </p>
+
+                    <Editor flix={this.props.flix}>
+                        {`pub def startsWith(s1: String, s2: String): Bool =
+    import java.lang.String.startsWith(String);
+    s1.startsWith(s2) as & Pure`}
+                    </Editor>
+
                 </SubSection>
 
                 <SubSection name="Invoking Object Methods">

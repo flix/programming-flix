43deac65b5826587aeaf6c9d5940c9c6299901c4
Magnus Madsen <magnus-madsen@users.noreply.github.com>
2021-05-13 09:14:32 +0200

    Revised Lists.flix.

--- src/content/Lists.js
+++ src/content/Lists.js
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactGA from "react-ga";
 
 import Code from '../components/Code';
-import Editor from '../util/Editor';
 import Section from "../components/Section";
+import CodeBlock from "../util/CodeBlock";
 
 class Lists extends React.Component {
 
@@ -14,69 +14,56 @@ class Lists extends React.Component {
 
     render() {
         return (
-            <Section name="Programming with Lists">
+            <Section name="Lists">
 
                 <p>
                     The bread and butter of functional programming is list processing. A list is either the empty list,
                     written as <Code>Nil</Code>, or a cons cell, written as <Code>x :: xs</Code> where <Code>x</Code> is
                     the head element and <Code>xs</Code> is the tail of the list. The <Code>List</Code> type is
                     polymorphic so you can have a list of integers, written as <Code>List[Int]</Code>, or a list of
-                    strings written as <Code>List[String]</Code>. The <Code>List</Code> type and list operations are part
+                    strings written as <Code>List[String]</Code>. The <Code>List</Code> type and list operations are
+                    part
                     of the Flix standard library.
                 </p>
 
-                <p> We can construct the empty list of integer as follows: </p>
-
-                <Editor flix={this.props.flix}>
-                    def main(): List[Int] = Nil
-                </Editor>
-
-                <p> And we can construct a list with the integers 1, 2, and 3 as follows: </p>
-
-                <Editor flix={this.props.flix}>
-                    def main(): List[Int] = 1 :: 2 :: 3 :: Nil
-                </Editor>
-
                 <p>
-                    We can also construct a list of strings with the
-                    strings <Code>"Hello"</Code> and <Code>"World"</Code>:
+                    We write the empty list as follows:
                 </p>
 
-                <Editor flix={this.props.flix}>
-                    def main(): List[String] = "Hello" :: "World" :: Nil
-                </Editor>
+                <CodeBlock>{`Nil`}</CodeBlock>
+
+                <p>
+                    We can construct a list of strings with the strings <Code>"Hello"</Code> and <Code>"World"</Code> as
+                    follows:
+                </p>
+
+                <CodeBlock>{`"Hello" :: "World" :: Nil`}</CodeBlock>
 
                 <p>Given a list there are many useful operations we can perform on it.</p>
 
-                <p>For example, we can compute the length of the list as follows:</p>
+                <p>For example, we can compute the length of a list as follows:</p>
 
-                <Editor flix={this.props.flix}>
-                    def main(): Int = List.length(1 :: 2 :: 3 :: Nil)
-                </Editor>
+                <CodeBlock>{`List.length(1 :: 2 :: 3 :: Nil)`}</CodeBlock>
 
-                <p>We can also reverse the order of elements in the list:</p>
+                <p>We can also reverse the order of elements in a list:</p>
 
-                <Editor flix={this.props.flix}>
-                    def main(): List[Int] = List.reverse(1 :: 2 :: 3 :: Nil)
-                </Editor>
+                <CodeBlock>{`List.reverse(1 :: 2 :: 3 :: Nil)`}</CodeBlock>
 
-                <p>We can append to lists using the <Code>List.append</Code> function as follows:</p>
+                <p>
+                    We can append two lists using the <Code>List.append</Code> function as follows:
+                </p>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[Int] =
-    let xs = (1 :: 2 :: 3 :: Nil);
-    let ys = (4 :: 5 :: 6 :: Nil);
-        List.append(xs, ys)`}
-                </Editor>
+                <CodeBlock>{`let xs = (1 :: 2 :: 3 :: Nil);
+let ys = (4 :: 5 :: 6 :: Nil);
+List.append(xs, ys)`}</CodeBlock>
 
-                <p>Or, alternatively, we can use the built-in append operator <Code>:::</Code> as follows:</p>
+                <p>
+                    Or, alternatively, we can use the built-in append operator <Code>:::</Code> as follows:
+                </p>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[Int] =
-    let xs = (1 :: 2 :: 3 :: Nil);
-    let ys = (4 :: 5 :: 6 :: Nil);
-        xs ::: ys`}
-                </Editor>
+                <CodeBlock>{`let xs = (1 :: 2 :: 3 :: Nil);
+let ys = (4 :: 5 :: 6 :: Nil);
+xs ::: ys`}</CodeBlock>
 
                 <p>
                     Flix has an extensive collection of functions to operate on lists.
@@ -86,60 +73,62 @@ class Lists extends React.Component {
                     Here are some of the most common:
                 </p>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): Int = List.count(x -> x == 1, 1 :: 2 :: 3 :: Nil)`}
-                </Editor>
+                <CodeBlock>
+                    {`List.count(x -> x == 1, 1 :: 2 :: 3 :: Nil)`}
+                </CodeBlock>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[Int] = List.filter(x -> x == 1, 1 :: 2 :: 3 :: Nil)`}
-                </Editor>
+                <CodeBlock>
+                    {`List.filter(x -> x == 1, 1 :: 2 :: 3 :: Nil)`}
+                </CodeBlock>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[Int] = List.map(x -> x + 1, 1 :: 2 :: 3 :: Nil)`}
-                </Editor>
+                <CodeBlock>
+                    {`List.map(x -> x + 1, 1 :: 2 :: 3 :: Nil)`}
+                </CodeBlock>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): Int = List.foldLeft((x, y) -> x + y, 0, 1 :: 2 :: 3 :: Nil)`}
-                </Editor>
+                <CodeBlock>
+                    {`List.foldLeft((x, y) -> x + y, 0, 1 :: 2 :: 3 :: Nil)`}
+                </CodeBlock>
 
                 <p>
                     And here are some more exotic:
                 </p>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[String] = List.intersperse("X", "a" :: "b" :: "c" :: Nil)`}
-                </Editor>
+                <CodeBlock>
+                    {`List.intersperse("X", "a" :: "b" :: "c" :: Nil)`}
+                </CodeBlock>
 
-                <Editor flix={this.props.flix}>
-                    {`def main(): List[String] =
-    let l1 = "X" :: "Y" :: Nil;
-    let l2 = ("a" :: "b" :: Nil) :: ("c" :: "d" :: Nil) :: Nil;
-    List.intercalate(l1, l2)`}
-                </Editor>
+                <p>
+                    which inserts <Code>"X"</Code> between every element in the list.
+                </p>
+
+                <CodeBlock>
+                    {`let l1 = "X" :: "Y" :: Nil;
+let l2 = ("a" :: "b" :: Nil) :: ("c" :: "d" :: Nil) :: Nil;
+List.intercalate(l1, l2)`}
+                </CodeBlock>
+
+                <p>
+                    which inserts the list <Code>l1</Code> between every element in the list <Code>l2</Code>.
+                </p>
 
                 <p>
                     We can write our own recursive functions to operate on lists.
                 </p>
 
                 <p>
-                    For example, here is the library implementation of <Code>List.map</Code>:
+                    For example, here is an implementation of the <Code>map</Code> function:
                 </p>
 
-                <Editor flix={this.props.flix}>
+                <CodeBlock>
                     {`///
 /// Returns the result of applying \`f\` to every element in \`xs\`.
 /// That is, the result is of the form: \`f(x1) :: f(x2) :: ...\`.
 ///
-pub def map[a,b](f: a -> b, xs: List[a]): List[b] = match xs with {
-    case Nil => Nil
+pub def map(f: a -> b & e, xs: List[a]): List[b] & e = match xs {
+    case Nil     => Nil
     case x :: rs => f(x) :: map(f, rs)
 }`}
-                </Editor>
-
-                <p>
-                    Before you write your own list function, be sure to check if it is already available in the
-                    library.
-                </p>
+                </CodeBlock>
 
             </Section>
         )
